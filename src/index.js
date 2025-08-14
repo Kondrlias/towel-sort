b3e6ae262d67db34861b5928e6b16c917df87133
Julia Kondratsiuk <kondrlias@gmail.com>
2025-08-14 17:16:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (typeof matrix === 'undefined' || matrix.length === 0) return [];
+  let result = [];
+  matrix.forEach((element, index) => {
+    if (index % 2 === 0) {
+      result = result.concat(element);
+    } else {
+      result = result.concat(element.reverse());
+    }
+  });
+
+  return result;
 };
